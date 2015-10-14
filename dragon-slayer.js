25bcaa786858743f246d20413ff1ed14b71bdd3d
Kevin Kendle <kevin.kendle09@gmail.com>
2015-10-13 23:25:30 -0500

    Added 3 potions for player to use. Enemy still has a random chance for potion. Need to limit potion usage and also make sure health doesn't go above max. Added crit checking. Currently just doubles attack damage for whichever player/enemy.

--- dragon-slayer.js
+++ dragon-slayer.js
@@ -5,12 +5,14 @@ function Character(name, health, attackPower) {
 }
 
 var person = new Character("Kevin", 10, 5);
-var opponent = new Character("Dragon", 15, 3);
+var opponent = new Character("Dragon", 25, 3);
 
 var turnCount = 1;
 
 var renderLog = document.getElementById("log");
 var renderTurn = document.getElementById("turn");
+var renderPlayerName = document.getElementById("playerName");
+var renderEnemyName = document.getElementById("enemyName");
 var renderPlayerHealth = document.getElementById("playerHealth");
 var renderEnemyHealth = document.getElementById("enemyHealth");
 var getPlayerHealthbar = document.getElementsByClassName("healthbar")[0];
@@ -46,48 +48,27 @@ var checkGameOver = function() {
 };
 
 var potionCheck = function(character) {
-  console.log("checking if using a potion");
-  console.log("this character " + character.name + " is checking for potion");
-  console.log(character.name + " has " + character.health + " left");
+  // check if {{ character passed in }} gets a potion
   var random = Math.random();
-  console.log(random);
   if(random <= .05) {
-    console.log("large potion");
-    console.log("10");
-    console.log(character.name + " has " + character.health + " health");
-    console.log(character.name + " is healed for 10 health and now has " + (character.health + 10) + " health")
-    return character.health += 10;
-  }
-  else if(random <= .08) {
-    console.log("medium potion");
-    console.log("7");
-    console.log(character.name + " has " + character.health + " health");
-    console.log(character.name + " is healed for 7 health and now has " + (character.health + 7) + " health")
-    return character.health += 7;
-  }
-  else if(random <= .15) {
-    console.log("small potion");
-    console.log("3");
-    console.log(character.name + " has " + character.health + " health");
-    console.log(character.name + " is healed for 3 health and now has " + (character.health + 3) + " health")
+    // small potion, 3 health
     return character.health += 3;
   }
   else {
-    console.log("no potion");
+    // no potion
     return 0;
   }
 }
 
 var hitCheck = function() {
-  // rewrite to raise hit rate.
   // may also add in dodge, parry, and separate block and miss.
   var hit = Math.random();
   if(hit <= .01) {
-    console.log("miss");
+    // miss
     return false;
   }
   else {
-    console.log("hit");
+    // hit
     return true;
   }
 };
@@ -97,35 +78,57 @@ var attack = function(number) {
   // probably under hit check
   // can be 0
   return Math.floor(Math.random() * number);
+};
+
+var critCheck = function() {
+  var random = Math.random();
+  console.log("checking for crit");
+  if(random <= .01) {
+    console.log("crit");
+    return true;
+  }
+  else {
+    console.log("no crit");
+    return false;
+  }
 }
+
+var usePotion = function(value) {
+  var getPotion = document.getElementsByClassName("potion");
+  if(value == 1) {
+    getPotion[0].classList.add("gray");
+    person.health += 3;
+    renderPlayerHealth.innerHTML = person.health;
+  }
+  else if(value == 2) {
+    getPotion[1].classList.add("gray");
+    person.health += 3;
+    renderPlayerHealth.innerHTML = person.health;
+  }
+  else if(value == 3) {
+    getPotion[2].classList.add("gray");
+    person.health += 3;
+    renderPlayerHealth.innerHTML = person.health;
+  }
+  return true;
+};
 
 
 
 renderTurn.innerHTML = (turnCount++);
+renderPlayerName.innerHTML = person.name;
+renderEnemyName.innerHTML = opponent.name;
 renderPlayerHealth.innerHTML = person.health;
 renderEnemyHealth.innerHTML = opponent.health;
 
 var playerTurn = function() {
   renderTurn.innerHTML = (turnCount++);
   scrollToBottom();
-  var potion = potionCheck(person);
-  console.log("potion return of " + potion);
-  if(potion > 0) {
-    console.log("healing");
-    renderPlayerHealth.innerHTML = person.health;
-  }
 
   renderLog.innerHTML += ("<br />");
   if(hitCheck()) {
-    console.log("player hit");
     var playerAttack = attack(person.attackPower);
-
-    if(opponent.health <= 12) {
-      getEnemyHealthbar.classList.add("yellow");
-    }
-    if(opponent.health <= 5) {
-      getEnemyHealthbar.classList.add("red");
-    }
+    console.log("player attack " + playerAttack);
 
     if(playerAttack == 0) {
       renderLog.innerHTML += ("The enemy blocked!<br/>");
@@ -133,7 +136,18 @@ var playerTurn = function() {
       enemyTurn();
     }
     else {
-      renderLog.innerHTML += ("You hit your enemy and did " + playerAttack + " damage<br/>");
+      var critAmount = critCheck();
+      console.log("crit return val " + critAmount);
+      if(critAmount) {
+        console.log("same attack val " + playerAttack);
+        playerAttack *= 2;
+        console.log("crit total damage " + playerAttack);
+        renderLog.innerHTML += ("Crit!<br />");
+        renderLog.innerHTML += ("You hit your enemy and did " + playerAttack + " damage<br/>");
+      }
+      else {
+        renderLog.innerHTML += ("You hit your enemy and did " + playerAttack + " damage<br/>");
+      }
       scrollToBottom();
       opponent.health -= playerAttack;
       renderEnemyHealth.innerHTML = opponent.health;
@@ -143,6 +157,12 @@ var playerTurn = function() {
         renderEnemyHealth.innerHTML = opponent.health;
         getEnemyHealthbar.classList.add("gray");
         playerWin();
+      }
+      else if(opponent.health <= 5) {
+        getEnemyHealthbar.classList.add("red");
+      }
+      if(opponent.health <= 12) {
+        getEnemyHealthbar.classList.add("yellow");
       }
       else {
         enemyTurn();
@@ -158,39 +178,47 @@ var playerTurn = function() {
 };
 
 var enemyTurn = function() {
-  var potion = potionCheck(opponent);
-  console.log("potion return of " + potion);
-  if(potion > 0) {
-    console.log("healing");
-    renderEnemyHealth.innerHTML = opponent.health;
-  }
-
   if(hitCheck()) {
-    console.log("enemy hit");
-    var enemyAttack = attack(opponent.attackPower);
+    // potion check only on hit
+    var potion = potionCheck(opponent);
+    if(potion > 0) {
+      renderEnemyHealth.innerHTML = opponent.health;
+    }
 
+    var enemyAttack = attack(opponent.attackPower);
+    console.log("enemy attack " + enemyAttack);
     if(enemyAttack == 0) {
       renderLog.innerHTML += ("You blocked!<br />");
       scrollToBottom();
     }
     else {
-      renderLog.innerHTML += ("The enemy hit you for " + enemyAttack + " damage<br />");
+      var critAmount = critCheck();
+      console.log("crit return val " + critAmount);
+      if(critAmount) {
+        console.log("same value " + enemyAttack);
+        enemyAttack *= 2;
+        console.log("crit total damage " + enemyAttack);
+        renderLog.innerHTML += ("Crit!<br />");
+        renderLog.innerHTML += ("The enemy hit you for " + enemyAttack + " damage<br/>");
+      }
+      else {
+        renderLog.innerHTML += ("The enemy hit you for " + enemyAttack + " damage<br />");
+      }
       scrollToBottom();
       person.health -= enemyAttack;
       renderPlayerHealth.innerHTML = person.health;
-
-      if(person.health <= 7) {
-        getPlayerHealthbar.classList.add("yellow");
-      }
-      if(person.health <= 3) {
-        getPlayerHealthbar.classList.add("red");
-      }
 
       if(person.health <= 0) {
         person.health = 0;
         renderPlayerHealth.innerHTML = person.health;
         getPlayerHealthbar.classList.add("gray");
         playerLose();
+      }
+      else if(person.health <= 3) {
+        getPlayerHealthbar.classList.add("red");
+      }
+      else if(person.health <= 7) {
+        getPlayerHealthbar.classList.add("yellow");
       }
       else {
         scrollToBottom();
